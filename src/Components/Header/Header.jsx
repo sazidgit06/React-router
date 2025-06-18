57b077f1b474b5aa13aae20699f9a4d998fe69c3
Sazid <sazidtec06@gmail.com>
2025-06-18 06:00:25 +0600

    module complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
             <h4>Navbar section</h4>
-            <ul>
-                <li><Link to="/">           Home    </Link></li>
-                <li><Link to="/about">      About   </Link></li>
-                <li><Link to="/contact">    Contact </Link></li>
-                <li><Link to="/users">    Users </Link></li>
-                <li><Link to="/photos">    Photos </Link></li>
-            </ul>
+            <nav>
+                <li><NavLink to="/">           Home    </NavLink></li>
+                <li><NavLink to="/about">      About   </NavLink></li>
+                <li><NavLink to="/contact">    Contact </NavLink></li>
+                <li><NavLink to="/users">    Users </NavLink></li>
+                <li><NavLink to="/photos">    Photos </NavLink></li>
+            </nav>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
             <h2>This is home component</h2>
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ?
+                <p>Loading...</p> :
+                    <Outlet></Outlet>
+            }
+
         </div>
     );
 };
